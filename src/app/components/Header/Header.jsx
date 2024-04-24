50a7c893d3939994807b1cddd4f916fdbbc29f7a
unknown <rishabh.pgpl@gmail.com>
2024-04-24 14:29:16 +0530

    Anti-Ragging Fixes

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,13 +1,23 @@
 'use client'
-// Import Image from "next/image";
 import Image from "next/image";
-import { useRouter } from "next/navigation"; // Import useRouter hook from next/router
 import React from "react";
 
 export default function Header() {
-  const router = useRouter(); // Use useRouter hook to access router object
-  const { college } = router.query || {}; // Destructure college query parameter or provide an empty object as default
-console.log(college)
+  // Get the college name from URL search parameters
+  const searchParams = new URLSearchParams(window.location.search);
+  const college = searchParams.get("college");
+
+  // Function to format the college name based on the query parameter
+  const formatCollegeName = (college) => {
+    // If college contains "-" (e.g., college-lncts), split it and format the name accordingly
+    if (college && college.includes("-")) {
+      const [collegeCode, collegeName] = college.split("-");
+      return `${collegeName} (${collegeCode.toUpperCase()})`;
+    }
+    // If college does not contain "-", return the college name in uppercase
+    return college ? `${college.toUpperCase()} College` : "College Name";
+  };
+
   return (
     <div className="px-20 py-2 flex gap-10 items-center">
       <div className="w-[370px] h-[120px] relative">
@@ -15,7 +25,7 @@ console.log(college)
       </div>
       <h1 className="text-4xl font-bold text-blue-600">
         {/* Use the college query parameter dynamically */}
-        {college ? `${college} College` : "College Name"}
+        {formatCollegeName(college)}
       </h1>
     </div>
   );
