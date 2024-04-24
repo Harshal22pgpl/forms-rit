2a808fc9fa0b6c2bdc2b73849380797b141ba776
unknown <rishabh.pgpl@gmail.com>
2024-04-24 14:36:55 +0530

    Anti-Ragging Fixes

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,22 +1,32 @@
 'use client'
 import Image from "next/image";
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-  // Get the college name from URL search parameters
-  const searchParams = new URLSearchParams(window.location.search);
-  const college = searchParams.get("college");
+  const [collegeName, setCollegeName] = useState("");
 
-  // Function to format the college name based on the query parameter
-  const formatCollegeName = (college) => {
-    // If college contains "-" (e.g., college-lncts), split it and format the name accordingly
-    if (college && college.includes("-")) {
-      const [collegeCode, collegeName] = college.split("-");
-      return `${collegeName} (${collegeCode.toUpperCase()})`;
+  useEffect(() => {
+    // Check if window object is defined (i.e., if the code is running on the client side)
+    if (typeof window !== "undefined") {
+      // Get the college name from URL search parameters
+      const searchParams = new URLSearchParams(window.location.search);
+      const college = searchParams.get("college");
+
+      // Function to format the college name based on the query parameter
+      const formatCollegeName = (college) => {
+        // If college contains "-" (e.g., college-lncts), split it and format the name accordingly
+        if (college && college.includes("-")) {
+          const [collegeCode, collegeName] = college.split("-");
+          return `${collegeName} (${collegeCode.toUpperCase()})`;
+        }
+        // If college does not contain "-", return the college name in uppercase
+        return college ? `${college.toUpperCase()} College` : "College Name";
+      };
+
+      // Set the formatted college name in state
+      setCollegeName(formatCollegeName(college));
     }
-    // If college does not contain "-", return the college name in uppercase
-    return college ? `${college.toUpperCase()} College` : "College Name";
-  };
+  }, []);
 
   return (
     <div className="px-20 py-2 flex gap-10 items-center">
@@ -24,8 +34,8 @@ export default function Header() {
         <Image src="/logo1.png" alt="logo" width={370} height={120} />
       </div>
       <h1 className="text-4xl font-bold text-blue-600">
-        {/* Use the college query parameter dynamically */}
-        {formatCollegeName(college)}
+        {/* Render the college name obtained from URL search parameters */}
+        {collegeName}
       </h1>
     </div>
   );
