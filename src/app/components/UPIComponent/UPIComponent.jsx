8a785ed8ecfd9982254d2e75593880ff94fe481a
unknown <rishabh.pgpl@gmail.com>
2024-05-25 11:41:08 +0530

    payments form

--- src/app/components/UPIComponent/UPIComponent.jsx
+++ src/app/components/UPIComponent/UPIComponent.jsx
@@ -1,14 +1,15 @@
 // components/UPIComponent.js
 import React from 'react';
+import Image from 'next/image';
 
 const UPIComponent = () => {
   const dummyUpiId = 'dummy@upi';
-  const dummyQrCode = '/qr'; // Replace with an actual path to the QR code image
+  const dummyQrCode = '/qr.jpg'; // Replace with an actual path to the QR code image
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg max-w-md mx-auto mt-8">
       <h3 className="text-xl font-bold mb-4">Pay via UPI</h3>
-      <img src={dummyQrCode} alt="QR Code" className="mx-auto mb-4" />
+      <Image src={dummyQrCode} alt="QR Code" className="mx-auto mb-4" />
       <p className="text-center mb-4">UPI ID: {dummyUpiId}</p>
       <p className="text-red-500 mb-4">Note: Mail your payment screenshot to the given email address.</p>
     </div>
